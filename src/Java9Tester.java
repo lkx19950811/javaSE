455a0cb9370d934d36990cafedfbeba27cffe7b8
lkx <lkx19950811@163.com>
2018-05-29 18:00:51 +0800

    1.写了几个流的新用法

--- src/Java9Tester.java
+++ src/Java9Tester.java
@@ -1,6 +1,13 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author leon
@@ -18,5 +25,32 @@ public class Java9Tester {
         Set.of(1, 2, 3);
         Map.of();
         Map.of("Hello", 1, "World", 2);
+    }
+    @Test
+    public void testDropWhile() throws Exception {//条件错误时剔除
+        final long count = Stream.of(1, 2, 3, 4, 5)
+                .dropWhile(i -> i % 2 != 0)
+                .count();
+        assertEquals(4, count);
+    }
+    @Test
+    public void takeWhile () throws Exception {//当条件对时留下
+        final long count = Stream.of(1, 2, 3, 4, 5)
+                .takeWhile (i -> i % 2 != 0)
+                .count();
+        assertEquals(1, count);
+    }
+    @Test
+    public void testFlatMapping() throws Exception { //新的流API
+        final Set<Integer> result = Stream.of("卧槽", "abc")
+                .collect(Collectors.flatMapping(v -> v.chars().boxed(),
+                        Collectors.toSet()));
+        assertEquals(5, result.size());
+        System.out.println(result);
+    }
+    private static final System.Logger LOGGER = System.getLogger("Main");
+    @Test
+    public void Testmain() {//java9 LOG
+        LOGGER.log(System.Logger.Level.INFO, "Run!");
     }
 }
